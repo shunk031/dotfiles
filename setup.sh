0068b725704849b62b5e75a1d3cc9fd25f1fa7d0
Shunsuke KITADA <septemtrio.ager@gmail.com>
2016-01-01 16:47:42 +0900

    add install.sh script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 
+echo '
+                          /$$                                      /$$      
+                         | $$                                     | $$      
+     /$$$$$$$  /$$$$$$  /$$$$$$   /$$   /$$  /$$$$$$      /$$$$$$$| $$$$$$$ 
+    /$$_____/ /$$__  $$|_  $$_/  | $$  | $$ /$$__  $$    /$$_____/| $$__  $$
+   |  $$$$$$ | $$$$$$$$  | $$    | $$  | $$| $$  \ $$   |  $$$$$$ | $$  \ $$
+    \____  $$| $$_____/  | $$ /$$| $$  | $$| $$  | $$    \____  $$| $$  | $$
+    /$$$$$$$/|  $$$$$$$  |  $$$$/|  $$$$$$/| $$$$$$$//$$ /$$$$$$$/| $$  | $$
+   |_______/  \_______/   \___/   \______/ | $$____/|__/|_______/ |__/  |__/
+                                           | $$                             
+                                           | $$                             
+                                           |__/
+   
+   *** This is dotfiles setup script ***
+   1. Download https://github.com/shunk031/dotfiles
+   2. Symlinking dot files to your home directory
+   3. To run install.sh in order to install the required application
+'
+
 # Create some symbolic link to home directory
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 echo "Created symbolic link of .bashrc to ~/"
@@ -13,6 +32,8 @@ echo "Created symbolic link of .Xresources to ~/"
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 echo "Created symbolic link of .zshrc to ~/"
 
+# called install.sh
+sh install.sh
 
 # install pyenv
 git clone https://github.com/yyuu/pyenv.git ~/.pyenv

ab76ee1a4440556b9ddc5e2b17ac06fb97809d12
Shunsuke KITADA <septemtrio.ager@gmail.com>
2016-11-07 03:39:39 +0900

    add notebookair setting script

---
+++ script/setup-notebookair-evn.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+gsettings set org.gnome.desktop.wm.keybindings switch-group '[]'
+gsettings set org.cinnamon.muffin.keybindings switch-group '[]'
+gsettings set org.cinnamon.desktop.keybindings.wm switch-group '[]'

18c5fc7a3ad5ba4bd4e385a6fd9fcbfd6bcc00d2
Shunsuke KITADA <septemtrio.ager@gmail.com>
2016-11-25 14:30:20 +0900

    add macbook like touchpad gesture settings

--- script/setup-notebookair-evn.sh
+++ script/setup-notebookair-evn.sh
@@ -44,7 +44,11 @@ nowsetup () {
     jot -b = -s "" ${n}
 }
 
-readonly FCITX_IMLIST_DIR=~/fcitx-imlist
+readonly DOTFILES_DIR=~/dotfiles
+readonly FCITX_IMLIST_DIR=$DOTFILES_DIR/script/fcitx-imlist
+readonly XSWIPE_DIR=$$DOTFILES_DIR/script/xSwipe
+readonly XSERVER_XORG_INPUT_SYNAPTICS_DIR=$DOTFILES_DIR/script/xserver-xorg-input-synaptics
+
 
 nowinstall "Disable switch-group keybindings"
 gsettings set org.gnome.desktop.wm.keybindings switch-group '[]'
@@ -54,7 +58,7 @@ echo "Disabled switch-group keybindings."
 
 nowinstall "Install fcitx-imlist"
 if [ -e $FCITX_IMLIST_DIR ]; then
-    echo -e "\nFcitx-imlist is alreader installed.\n"
+    echo -e "\nFcitx-imlist is already installed.\n"
 else
     
     sudo apt-get install build-essential fcitx-libs-dev
@@ -69,4 +73,26 @@ else
     gsettings set org.xdump.fcitximlist fcitx-imlist-alternative 'us,mozc'
 fi
 
+
+nowinstall "Setup Macbook like gestures"
+# clone xSwipe repository and install libx11-guitest-perl
+if [ -e $XSWIPE_DIR ]; then
+    echo -e "\nxSwipe is already installed.\n"
+else
+    git clone https://github.com/iberianpig/xSwipe.git
+    sudo apt-get install libx11-guitest-perl
+fi
+# clone xserver-xorg-input-synpatics and install the app
+if [ -e $XSERVER_XORG_INPUT_SYNAPTICS_DIR ]; then
+    echo -e "\nxserver-xorg-input-synaptics is already installed.\n"
+else
+    sudo apt-get install -y git build-essential libevdev-dev autoconf automake libmtdev-dev xorg-dev xutils-dev libtool
+    sudo apt-get remove -y xserver-xorg-input-synaptics
+    git clone https://github.com/Chosko/xserver-xorg-input-synaptics.git
+    cd xserver-xorg-input-synaptics
+    ./autogen.sh
+    ./configure --exec_prefix=/usr
+    make
+    sudo make install
+fi
 

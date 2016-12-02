bff1b0332c7e6a42bfab7a78609b4dfaad73933d
Shunsuke KITADA <septemtrio.ager@gmail.com>
2016-12-02 22:24:43 +0900

    remove setting of installing oracle java8 instaler

--- os-env/setup-linuxmint-env.sh
+++ os-env/setup-linuxmint-env.sh
@@ -153,10 +153,10 @@ sudo apt-get install -y python-pip
 
 
 
-nowinstall "Install JDK 8"
-sudo add-apt-repository -y ppa:webupd8team/java
-sudo apt-get update
-sudo apt-get install -y oracle-java8-installer
+# nowinstall "Install JDK 8"
+# sudo add-apt-repository -y ppa:webupd8team/java
+# sudo apt-get update
+# sudo apt-get install -y oracle-java8-installer
 
 
 

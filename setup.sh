39710898471b8281f21c23354ae61171eb69ccd8
Shunsuke KITADA <shunk031@CyberAngelMacbook.local>
2017-11-03 00:40:42 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,13 +14,10 @@ setupsh_logo='
                                            |__/
    
    *** This is dotfiles setup script ***
-
    1. Download https://github.com/shunk031/dotfiles
    2. Symlinking dot files to your home directory
    3. Setup Prezto
-   4. To run install.sh in order to install the required application
-   5. Setup geeknote -- evernote client
-   6. Install Ricty -- fonts for programinng
+   4. Setup tpm | tmux plugin manager
 '
 
 readonly DOTFILES_DIR=~/dotfiles
@@ -45,47 +42,37 @@ fi
 
 
 # Create some dotfile symbolic links to home directory
-ln -sfn $DOTFILES_DIR/bashrc ~/.bashrc
-echo "[ SYMLINK ] Created symbolic link of bashrc to home directory"
-
-ln -sfn $DOTFILES_DIR/profile ~/.profile
-echo "[ SYMLINK ] Created symbolic link of profile to home directory"
-
-ln -sfn $DOTFILES_DIR/Xresources ~/.Xresources
-echo "[ SYMLINK ] Created symbolic link of Xresources to home directory"
-
-ln -sfn $DOTFILES_DIR/xinitrc ~/.xinitrc
-echo "[ SYMLINK ] Created symbolic link of xinitrc to home directory"
-
-ln -sfn $DOTFILES_DIR/xsession ~/.xsession
-echo "[ SYMLINK ] Created symbolic link of xsession to home directory"
-
-ln -sfn $DOTFILES_DIR/zshrc ~/.zshrc
+ln -sfn $DOTFILES_DIR/prezto.d/zshrc.zsh ~/.zshrc
 echo "[ SYMLINK ] Created symbolic link of zshrc to home directory"
 
-ln -sfn $DOTFILES_DIR/zlogin ~/.zlogin
+ln -sfn $DOTFILES_DIR/prezto.d/zlogin.zsh ~/.zlogin
 echo "[ SYMLINK ] Created symbolic link of zlogin to home directory"
 
-ln -sfn $DOTFILES_DIR/zlogout ~/.zlogout
+ln -sfn $DOTFILES_DIR/prezto.d/zlogout.zsh ~/.zlogout
 echo "[ SYMLINK ] Created symbolic link of zlogout to home directory"
 
-ln -sfn $DOTFILES_DIR/zpreztorc ~/.zpreztorc
+ln -sfn $DOTFILES_DIR/prezto.d/zpreztorc.zsh ~/.zpreztorc
 echo "[ SYMLINK ] Created symbolic link of zpreztorc to home directory"
 
-ln -sfn $DOTFILES_DIR/zprofile ~/.zprofile
+ln -sfn $DOTFILES_DIR/prezto.d/zprofile.zsh ~/.zprofile
 echo "[ SYMLINK ] Created symbolic link of zprofile to home directory"
 
-ln -sfn $DOTFILES_DIR/zshenv ~/.zshenv
+ln -sfn $DOTFILES_DIR/prezto.d/zshenv.zsh ~/.zshenv
 echo "[ SYMLINK ] Created symbolic link of zshenv to home directory"
 
-ln -sfn $DOTFILES_DIR/Xmodmap ~/.Xmodmap
-echo "[ SYMLINK ] Created symbolic link of Xmodmap to home directory"
-
-ln -sfn $DOTFILES_DIR/tmux.conf ~/.tmux.conf
+ln -sfn $DOTFILES_DIR/tmux.d/tmux.conf ~/.tmux.conf
 echo "[ SYMLINK ] Created symbolic link of tmux.conf to home directory"
 
-ln -sfn $DOTFILES_DIR/gitconfig ~/.gitconfig
+ln -sfn $DOTFILES_DIR/tmux.d/tmux-powerlinerc.zsh ~/.tmux-powerlinerc
+echo "[ SYMLINK ] Created symbolic link fo tmux-powerlinerc to home directory"
+
+ln -sfn $DOTFILES_DIR/git_config/gitignore_global ~/.gitignore_global
 echo "[ SYMLINK ] Created symbolic link of gitconfig to home directory"
+
+ln -sfn　$DOTFILES_DIR/emacs.d $DOTEMACS_DIR
+echo "[ SYMLINK ] Created symbolic link of emacs.d to home directory"
+
+
 
 # Create devilspie symbolic link to home directory
 if [[ "$OSTYPE" =~ linux-gnu ]]; then
@@ -107,27 +94,11 @@ else
     git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
     setopt EXTENDED_GLOB
 fi
+
 # Create some symblic link to ~/.prezto
-ln -sfn $DOTFILES_DIR/prezto.d/prompt_sorin_setup ~/.zprezto/modules/prompt/functions/prompt_sorin_setup
-echo "[ SYMLINK ] Created symbolic link of prompt_sorin_setup to ~/.zprezto"
-
-ln -sfn $DOTFILES_DIR/prezto.d/prompt_my_sorin_setup ~/.zprezto/modules/prompt/functions/prompt_my_sorin_setup
-echo "[ SYMLINK ] Created symbolic link of prompt_my_sorin_setup to ~p/.zprezto"
-
-ln -sfn $DOTFILES_DIR/prezto.d/prompt_my_powerline_setup ~/.zprezto/modules/prompt/functions/prompt_my_powerline_setup
+ln -sfn $DOTFILES_DIR/prezto.d/prompt/prompt_my_powerline_setup.zsh ~/.zprezto/modules/prompt/functions/prompt_my_powerline_setup
 echo "[ SYMLINK ] Created symbolic link of prompt_my_powerline_setup to ~/.zprezto"
-    
 
-
-
-
-# Clone my emacs.d
-if [ -e $DOTEMACS_DIR ];then
-    echo -e "[ INSTALL ] emacs.d is already cloned."
-else
-    git clone https://github.com/shunk031/emacs.d.git $DOTEMACS_DIR
-    bash ~/emacs.d/setup.sh
-fi
 
 
 # Clone tpm(tmux plugin manager)
@@ -136,14 +107,3 @@ if [ -e $TPM_DIR ]; then
 else
     git clone https://github.com/tmux-plugins/tpm $TPM_DIR
 fi
-
-
-# Install font "Ricty"
-ls $RICTY_FILE > /dev/null 2>&1
-if [ $? -ne 0 ]; then
-    echo "[ INSTALL ] Now install Ricty..."
-    bash $DOTFILES_DIR/script/setup-ricty.sh
-else
-    echo -e "[ INSTALL ] Ricty is already installed.\n"
-fi
-

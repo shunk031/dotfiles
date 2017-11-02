6d24f847e1504f9e529f38f10af3c1de23429fed
Shunsuke KITADA <shunk031@CyberAngelMacbook.local>
2017-11-03 00:40:23 +0900

    delete old dotfiles

--- os-env/setup-pyenv.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev
-
-git clone https://github.com/yyuu/pyenv.git ~/.pyenv
-
-git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv

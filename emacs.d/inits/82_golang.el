98c1c5c42b80845ea73cd80273d2a10a836eb172
Shunsuke KITADA <septemtrio.ager@gmail.com>
2017-11-05 16:58:07 +0900

    update golang.el

--- emacs.d/inits/82_golang.el
+++ emacs.d/inits/82_golang.el
@@ -2,9 +2,8 @@
 (el-get-bundle nsf/gocode)
 
 (use-package company-go
+  :init
   (add-hook 'go-mode-hook (lambda ()
 			    (set (make-local-variable 'company-backends) '(company-go))
 			    (company-mode)))
   )
-
-

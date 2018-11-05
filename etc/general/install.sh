1f5aad00ae70e3f2719489594715fc7b1dc69385
Shunsuke KITADA <shunk031@OguraYuiMacbook.local>
2018-11-05 14:38:15 +0900

    update

--- etc/general/install.sh
+++ etc/general/install.sh
@@ -1,17 +1,23 @@
 POWERLINE_FONT_DIR=${DOTPATH}/.github/powerline_fonts
 
+install_powerline_font() {
+    e_newline
+    e_header "Installing powerline font..."
+
+    git clone -q https://github.com/powerline/fonts.git $POWERLINE_FONT_DIR --depth=1
+    ${POWERLINE_FONT_DIR}/install.sh
+
+    e_newline && e_done "Install powerline"
+}
+
 install_prezto () {
     e_newline
     e_header "Installing prezto..."
 
     git clone -q --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
     ln -sfnv ${DOTPATH}/.zsh/prezto/prompt_my_powerline_setup.zsh ${HOME}/.zprezto/modules/prompt/functions/prompt_my_powerline_setup
+
+    e_newline && e_done "Install prezto"
 }
 
-install_powerline_font() {
-    e_newline
-    e_header "Installing powerline font..."
 
-    git clone -q https://github.com/powerline/fonts.git $POWERLINE_FONT_DIR --depth=1
-    ./${POWERLINE_FONT_DIR}/install.sh
-}

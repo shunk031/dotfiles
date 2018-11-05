3e2404e4102fae427e0490ff7b4b7c05c18680a9
Shunsuke KITADA <shunk031@OguraYuiMacbook.local>
2018-11-05 15:45:57 +0900

    remove

--- .zsh/20_rbenv.sh
+++ .zsh/20_rbenv.sh
@@ -1,17 +1,3 @@
-RBENV_DIR=${HOME}/.rbenv
-
-if [ ! -e "$RBENV_DIR" ]; then
-    e_header "Installing rbenv..."
-    git clone https://github.com/rbenv/rbenv.git $RBENV_DIR
-    ~/.rbenv/src/configure && make -C src
-fi
-
-if ! has 'ruby-build'; then
-    e_header "Installing ruby-build..."
-    mkdir -p "$(rbenv root)"/plugins
-    git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
-fi
-
 # Setup rbenv environment
 export PATH="$HOME/.rbenv/bin:$PATH"
 export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"

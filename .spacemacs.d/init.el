b48ff92a95d819f4a6fb2128018a78ea537a7498
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2018-11-08 21:01:38 +0900

    exclud evil-search-highlight-persist

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -91,7 +91,9 @@ values."
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
    ;; A list of packages that will not be installed and loaded.
-   dotspacemacs-excluded-packages '()
+   dotspacemacs-excluded-packages '(
+                                    evil-search-highlight-persist
+                                    )
    ;; Defines the behaviour of Spacemacs when installing packages.
    ;; Possible values are `used-only', `used-but-keep-unused' and `all'.
    ;; `used-only' installs only explicitly used packages and uninstall any

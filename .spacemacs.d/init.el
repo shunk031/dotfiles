e9f84632771f5a05ea8a63da62d9fa1a5eb4aba7
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2018-12-09 11:52:44 +0900

    set default tab-width 4

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -361,6 +361,7 @@ you should place your code here."
   (blink-cursor-mode 1)
   (flycheck-pos-tip-mode nil)
 
+  (setq-default tab-width 4)
   (setq company-idle-delay 0.1)
   (setq mac-command-modifier 'meta)
 

deb6e384f282b3e26d4824629574ac8c774fa131
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2019-01-08 13:20:12 +0900

    add my-python-config

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -75,6 +75,7 @@ values."
      tabbar
      yatex
      japanese
+     my-python-config
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these

0d1f10471bacff4e351e60c0fb31540f695d4f01
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2019-05-16 07:32:18 +0900

    use python-language-server=="0.22.0"

--- bin/setup-pydev-env.sh
+++ bin/setup-pydev-env.sh
@@ -4,7 +4,7 @@ pip install -U --no-cache-dir \
     "jedi>=0.9.0" \
     "json-rpc>=1.8.1" \
     "service_factory>=0.1.5" \
-    python-language-server \
+    "python-language-server==0.22.0" \
     pyls-isort \
     flake8 \
     black \

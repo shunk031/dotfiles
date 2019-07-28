6cc57fa1b3cdc1b88c3fc7f038c50065b3259e8e
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2019-07-28 10:45:28 +0900

    add counsel-flycheck

--- .spacemacs.d/layers/my-ivy-config/funcs.el
+++ .spacemacs.d/layers/my-ivy-config/funcs.el
@@ -17,3 +17,25 @@
     (overlay-put overlay 'window wnd)
     (overlay-put overlay 'priority priority)
     overlay))
+
+(defun counsel-flycheck ()
+    (interactive)
+    (if (not (bound-and-true-p flycheck-mode))
+      (message "Flycheck mode is not available or enabled")
+      (ivy-read "Error: "
+        (let ((source-buffer (current-buffer)))
+          (with-current-buffer (or (get-buffer flycheck-error-list-buffer)
+                                 (progn
+                                   (with-current-buffer
+                                     (get-buffer-create flycheck-error-list-buffer)
+                                     (flycheck-error-list-mode)
+                                     (current-buffer))))
+            (flycheck-error-list-set-source source-buffer)
+            (flycheck-error-list-reset-filter)
+            (revert-buffer t t t)
+            (split-string (buffer-string) "\n" t " *")))
+        :action (lambda (s &rest _)
+                  (-when-let* ( (error (get-text-property 0 'tabulated-list-id s))
+                                (pos (flycheck-error-pos error)) )
+                    (goto-char (flycheck-error-pos error))))
+        :history 'counsel-flycheck-history)))

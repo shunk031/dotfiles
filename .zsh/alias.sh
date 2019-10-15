61c90e7507aa6e6f3cc4ea7910685bb0a6e38e9b
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2019-10-15 11:26:41 +0900

    add `resetport` alias

--- .zsh/alias.sh
+++ .zsh/alias.sh
@@ -1,3 +1,4 @@
 # for finding directory of neologd dictionary
 # alias which-neologd='echo `mecab-config --dicdir`"/mecab-ipadic-neologd"'
 alias ns='watch -n 1 "nvidia-smi"'
+alias resetport='lsof -t -i tcp:8000 | xargs kill -9'

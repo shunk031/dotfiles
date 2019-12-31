2838beb9b4f981420160a3e42cb81f0ab25fa846
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2020-01-01 03:20:28 +0900

    update

--- tests/shellcheck.sh
+++ tests/shellcheck.sh
@@ -9,7 +9,7 @@ main() {
     find \
         ../install \
         ../tests \
-        ! -path ../install/macos/*.plist \
+        ! -path '../install/macos/*.plist' \
         -type f \
         -exec shellcheck \
         -e SC1090 \

1e7220c02efb9d6dfa5e19dab4861ca06ee32869
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2020-03-04 20:29:12 +0900

    fix for CI

--- install/macos/vscode.sh
+++ install/macos/vscode.sh
@@ -12,7 +12,7 @@ install_vscode() {
 
     declare -r VSCODE_SETTINGS="${VSCODE_DIR}"/settings.json
     declare -r VSCODE_KEYBINDINGS="${VSCODE_DIR}"/keybindings.json
-    
+
     declare -r CODE_COMMAND="/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code"
 
     brew_install "Visual Studio Code" "visual-studio-code" "homebrew/cask" "cask"
@@ -34,10 +34,10 @@ install_vscode() {
 
     cat "${VSCODE_DIR}/extensions" | grep -v '^#' | xargs -L1 ${CODE_COMMAND} --install-extension
 
-    execute \
-        "cat ${VSCODE_DIR}/extensions | grep -v '^#' | xargs -L1 ${CODE_COMMAND} --install-extension" \
-        "Install extensions from ${VSCODE_DIR}/extensions" \
-        || return 1
+    # execute \
+    #     "cat ${VSCODE_DIR}/extensions | grep -v '^#' | xargs -L1 ${CODE_COMMAND} --install-extension" \
+    #     "Install extensions from ${VSCODE_DIR}/extensions" \
+    #     || return 1
 }
 
 main() {

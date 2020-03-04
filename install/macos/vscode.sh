5e1545ce8808a4d51af8fcff8ba017317d4e9c2b
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2020-03-04 14:34:44 +0900

    add install script for setup.sh

---
+++ install/macos/vscode.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "${DOTPATH}"/install/util.sh \
+    && . "${DOTPATH}"/install/macos/util.sh
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+install_vscode() {
+    declare -r VSCODE_DIR="${DOTPATH}"/.vscode.d
+    declare -r APP_DIR="~/Library/Application\ Support/Code/User"
+
+    brew_install "Visual Studio Code" "visual-studio-code" "caskroom/cask" "cask"
+
+    execute \
+        "ln -sf ${VSCODE_DIR} ${APP_DIR}" \
+        "Symbolic link ${VSCODE_DIR} to ${APP_DIR}" \
+        || return 1
+
+    execute \
+        "cat ${VSCODE_DIR}/extensions | grep -v '^#' | xargs -L1 code --install-extension" \
+        "Install extensions from ${VSCODE_DIR}/extensions" \
+        || return 1
+}
+
+main() {
+    print_in_purple "\n   Install VSCode\n\n"
+    install_vscode
+}
+
+main

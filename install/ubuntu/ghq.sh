0238afa0925d65f13c7150cfba46c556d6f55d98
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2020-10-11 01:07:43 +0900

    disable goenv

--- install/ubuntu/ghq.sh
+++ install/ubuntu/ghq.sh
@@ -22,8 +22,6 @@ install_ghq() {
 
     install_package "golang ${GOLANG_VERSION}" "golang-go"
 
-    go version
-
     execute \
         "go get github.com/motemen/ghq && mkdir -p ${HOME}/ghq" \
         "ghq" \

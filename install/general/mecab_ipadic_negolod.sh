96aa402572be854822258524adf331806a28b18e
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2021-01-15 22:53:05 +0900

    add and update for mecab-ipadic-neologd

---
+++ install/general/mecab_ipadic_negolod.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "${DOTPATH}"/install/util.sh
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+install_mecab_ipadic_negolod() {
+    declare -r MECAB_IPADIC_NEOLOGD_DIR="/tmp"
+    declare -r MECAB_IPADIC_NEOLOGD_URL="https://github.com/neologd/mecab-ipadic-neologd.git"
+
+    execute \
+        "git clone --quiet --depth 1 ${MECAB_IPADIC_NEOLOGD_URL} ${MECAB_IPADIC_NEOLOGD_DIR}" \
+        "Clone to ${MECAB_IPADICT_NEOLOGD_DIR}" \
+        || return 1
+
+    execute \
+        "cd ${MECAB_IPADIC_NEOLOGD_DIR} \
+            && ./bin/install-mecab-ipadic-neologd -n" \
+        "Install" \
+        || return 1
+}
+
+main() {
+    print_in_purple "\n   mecab-ipadic-neologd\n\n"
+    install_mecab_ipadic_negolod
+}
+
+main
---
+++ install/macos/mecab.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "${DOTPATH}"/install/util.sh \
+    && . "${DOTPATH}"/install/macos/util.sh
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+install_mecab() {
+    brew_install "mecab" "mecab"
+    brew_install "mecab-ipadic" "mecab-ipadic"
+}
+
+main() {
+    print_in_purple "\n   mecab\n\n"
+
+    install_mecab
+    bash "${DOTPATH}"/install/general/mecab_ipadic_neologd.sh
+}

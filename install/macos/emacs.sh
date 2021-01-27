cc9ac17a4a2ac880d42b345aba262c6a3938322e
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2021-01-27 15:21:58 +0900

    update

--- install/macos/emacs.sh
+++ install/macos/emacs.sh
@@ -9,11 +9,14 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 install_emacs() {
 
-    declare -r EMACS_DIR="/usr/local/opt/emacs-plus/Emacs.app"
-    declare -r APP_DIR="/Applications"
+    declare -r APP_DIR="~/Applications"
+
+    declare -r EMACS_PLUS_VERSION="28"
+    declare -r EMACS_VERSION="${EMACS_PLUS_VERSION}.0.50"
+    declare -r EMACS_DIR="$(brew --prefix)/Cellar/emacs-plus@${EMACS_PLUS_VERSION}/${EMACS_VERSION}/Emacs.app"
 
     brew_tap "d12frosted/emacs-plus"
-    brew_install "Emacs plus" "emacs-plus"
+    brew_install "Emacs plus" "emacs-plus@${EMACS_PLUS_VERSION}"
 
     execute \
         "ln -sf ${EMACS_DIR} ${APP_DIR}" \

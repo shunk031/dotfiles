b59a9d7d59745b7660e62c4a0f965946c811395c
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2021-05-09 00:18:10 +0900

    add alias for gcloud

--- .zsh/alias.sh
+++ .zsh/alias.sh
@@ -3,3 +3,4 @@
 # alias ns='watch -n 1 "nvidia-smi"'
 # alias resetport='lsof -t -i tcp:8000 | xargs kill -9'
 alias ls="exa --long --group --header --binary --time-style=long-iso --icons"
+alias gcloud="docker run --rm -it -v $HOME/.config/gcloud:/root/.config/gcloud gcr.io/google.com/cloudsdktool/cloud-sdk gcloud"

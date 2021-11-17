8926df188eb4203cbd5b96a84b6eaff1a1998391
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2021-11-17 21:48:18 +0900

    update opt_out_of_analytics method (#59)

--- install/macos/homebrew.sh
+++ install/macos/homebrew.sh
@@ -35,24 +35,7 @@ install_homebrew() {
 
 opt_out_of_analytics() {
 
-    local path=""
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # Try to get the path of the `Homebrew` git config file.
-
-    path="$(get_homebrew_git_config_file_path)" \
-        || return 1
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # Opt-out of Homebrew's analytics.
-    # https://github.com/Homebrew/brew/blob/0c95c60511cc4d85d28f66b58d51d85f8186d941/share/doc/homebrew/Analytics.md#opting-out
-
-    if [ "$(git config --file="$path" --get homebrew.analyticsdisabled)" != "true" ]; then
-        git config --file="$path" --replace-all homebrew.analyticsdisabled true &> /dev/null
-    fi
-
+    brew analytics off
     print_result $? "Homebrew (opt-out of analytics)"
 
 }

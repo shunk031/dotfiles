6495e0748e8dd387d4802ed0e8d215689e0a6f77
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2022-01-10 01:08:49 +0900

    add aliases (#64)

--- .zsh/alias.sh
+++ .zsh/alias.sh
@@ -8,3 +8,6 @@ alias gcloud="docker run --rm -it -v $HOME/.config/gcloud:/root/.config/gcloud g
 # delete all git branches which have been "squash and merge" via GitHub
 # ref. https://stackoverflow.com/questions/43489303/how-can-i-delete-all-git-branches-which-have-been-squash-and-merge-via-github
 alias gprunesquashmerged='git checkout -q master && git for-each-ref refs/heads/ "--format=%(refname:short)" | while read branch; do mergeBase=$(git merge-base master $branch) && [[ $(git cherry master $(git commit-tree $(git rev-parse "$branch^{tree}") -p $mergeBase -m _)) == "-"* ]] && git branch -D $branch; done'
+
+# alias for bash-language-server in docker container
+alias bash-language-server="docker run --rm -i bash-language-server:latest"
--- .zsh/macos.sh
+++ .zsh/macos.sh
@@ -1,2 +1,6 @@
 # Homebrew will not auto-update before running `brew install`
 export HOMEBREW_NO_AUTO_UPDATE=1
+
+# alias for tailscale
+alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
+

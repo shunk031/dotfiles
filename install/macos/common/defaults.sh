43437decbeb871638ef9ff759f1aa2e19728adec
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2023-01-04 19:42:32 +0900

    update

--- install/macos/common/defaults.sh
+++ install/macos/common/defaults.sh
@@ -20,6 +20,7 @@ function defaults_trackpad() {
     defaults write -g com.apple.trackpad.scaling 2
 
     # Trackpad: enable tap to click for this user and for the login screen
+    defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
     defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
     defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
     defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1

1a2295a6745cbcc4a20a44c32bea274e24d10183
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2023-01-05 16:45:55 +0900

    update

--- setup.sh
+++ setup.sh
@@ -22,16 +22,54 @@ declare -r DOTFILES_LOGO='
 
 declare -r DOTFILES_REPO_URL="https://github.com/shunk031/dotfiles"
 
-function initialize_dotfiles() {
+function install_xcode() {
 
+    local git_cmd_path="/Library/Developer/CommandLineTools/usr/bin/git"
+
+    if [ ! -e "${git_cmd_path}" ]; then
+        # Install command line developer tool
+        xcode-select --install
+        # Want for user input
+        echo "Press any key when the installation has completed."
+        IFS= read -r -n 1 -d ''
+        #          │  │    └ The first character of DELIM is used to terminate the input line, rather than newline.
+        #          │  └ returns after reading NCHARS characters rather than waiting for a complete line of input.
+        #          └ If this option is given, backslash does not act as an escape character. The backslash is considered to be part of the line. In particular, a backslash-newline pair may not be used as a line continuation.
+    else
+        echo "Command line developer tools are installed."
+    fi
+}
+
+function initialize_macos() {
+    install_xcode
+    echo "Finish to pre-initialize MacOS"
+}
+
+function initialize_linux() {
+    echo "Finish to pre-initialize Linux OS"
+}
+
+function initialize_os_env() {
+    local ostype
+    ostype="$(uname)"
+
+    if [ "${ostype}" == "Darwin" ]; then
+        initialize_macos
+    elif [ "${ostype}" == "Linux" ]; then
+        initialize_linux
+    else
+        echo "Invalid OS type: ${ostype}"
+        exit 1
+    fi
+}
+
+function initialize_dotfiles() {
     function run_chezmoi() {
         sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply "${DOTFILES_REPO_URL}"
     }
-
     function cleanup_chezmoi() {
         rm -f "${HOME}/bin/chezmoi"
     }
-
     run_chezmoi
     cleanup_chezmoi
 }
@@ -75,8 +113,9 @@ function restart_shell() {
 function main() {
     echo "$DOTFILES_LOGO"
 
-    initialize_dotfiles
-    restart_shell
+    initialize_os_env
+    # initialize_dotfiles
+    # restart_shell
 }
 
 main "$@"

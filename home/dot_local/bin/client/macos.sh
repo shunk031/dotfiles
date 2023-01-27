18bbf93fad14127114970afd5ed9154e08091941
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2023-01-27 16:14:36 +0900

    update

--- home/dot_local/bin/client/macos.sh
+++ home/dot_local/bin/client/macos.sh
@@ -10,4 +10,5 @@ export HOMEBREW_NO_AUTO_UPDATE=1
 alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
 
 # for lima docker
-export DOCKER_HOST=$(limactl list docker_x86_64 --format 'unix://{{.Dir}}/sock/docker.sock')
+DOCKER_HOST=$(limactl list docker_x86_64 --format 'unix://{{.Dir}}/sock/docker.sock')
+export DOCKER_HOST

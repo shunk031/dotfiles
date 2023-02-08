4c82dc20ceefcebce867bf6deae3bccbfd341201
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2023-02-09 01:11:01 +0900

    add `--no-modify-path` option

--- install/common/rust.sh
+++ install/common/rust.sh
@@ -9,7 +9,7 @@ fi
 function install_rust() {
     # Install rust using rustup
     # ref. https://www.rust-lang.org/tools/install
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --no-modify-path
 }
 
 function main() {

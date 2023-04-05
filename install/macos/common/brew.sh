b5a6a894435eb189b5f502648dbd4bfbd98e8c64
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2023-04-05 17:56:58 +0900

    update `brew.sh`

--- install/macos/common/brew.sh
+++ install/macos/common/brew.sh
@@ -14,10 +14,6 @@ function install_homebrew() {
     if ! is_homebrew_exists; then
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     fi
-
-    if [ -e "/opt/homebrew/bin/brew" ]; then
-        eval "$(/opt/homebrew/bin/brew shellenv)"
-    fi
 }
 
 function opt_out_of_analytics() {

9905ac25310634b2a16ddcd53650780ca60b0379
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2023-06-15 15:51:13 +0900

    add `login-ghcr` command

--- home/dot_local/bin/client/alias.sh
+++ home/dot_local/bin/client/alias.sh
@@ -7,5 +7,5 @@
 alias ls="exa --long --group --header --binary --time-style=long-iso --icons"
 alias gcloud='docker run --rm -it -v "${HOME%/}"/.config/gcloud:/root/.config/gcloud gcr.io/google.com/cloudsdktool/cloud-sdk gcloud'
 
-# # alias for bash-language-server in docker container
+# alias for bash-language-server in docker container
 alias bash-language-server="docker run --rm -i bash-language-server:latest"

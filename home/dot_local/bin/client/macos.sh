eb847ca6340a78dbdb90f0292926e8f499aaf1b4
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2023-08-10 20:46:22 +0900

    add scripts

--- home/dot_local/bin/client/macos.sh
+++ home/dot_local/bin/client/macos.sh
@@ -7,4 +7,5 @@ export HOMEBREW_NO_AUTO_UPDATE=1
 alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
 
 # for lima docker
-export DOCKER_HOST="unix://$HOME/.colima/docker.sock"
+# export DOCKER_HOST="unix://$HOME/.colima/docker.sock"
+export DOCKER_HOST=unix:///var/run/docker.sock

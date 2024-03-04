70c3bfbec4fae58759e6bfac8cf555a46231ea0d
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2024-03-04 11:44:41 +0900

    update for command history

--- home/dot_local/bin/server/history.zsh
+++ home/dot_local/bin/server/history.zsh
@@ -1,6 +1,11 @@
 #!/usr/bin/env zsh
 
+setopt share_history
+setopt append_history
 setopt inc_append_history
+setopt hist_no_store
+setopt hist_ignore_all_dups
+setopt hist_ignore_dups
 
 export HISTFILE=${HISTFILE:-$HOME}/.zsh_history
 export HISTSIZE=10000

eeff84cf117cebe0698dc25c2c4252a8d95f5867
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2024-03-05 17:44:07 +0900

    update for eza and exa

--- install/ubuntu/common/eza.sh
+++ install/ubuntu/common/eza.sh
@@ -6,11 +6,24 @@ if [ "${DOTFILES_DEBUG:-}" ]; then
     set -x
 fi
 
+function apt_install_eza() {
+    sudo apt install -y eza
+}
+
+function apt_install_exa() {
+    sudo apt install -y exa
+}
+
 function install_eza() {
-    wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo tee /etc/apt/trusted.gpg.d/gierens.asc
-    echo "deb http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
-    sudo apt-get update
-    sudo apt-get install -y eza
+    sudo mkdir -p /etc/apt/keyrings
+    wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --yes --dearmor -o /etc/apt/keyrings/gierens.gpg
+    echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+    sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+    sudo apt update
+
+    if ! apt_install_eza; then
+        apt_install_exa
+    fi
 }
 
 function uninstall_eza() {

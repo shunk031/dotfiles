228e9e30407a2b8eda7d17f90ed83b9c5c1fc19c
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2024-11-05 18:09:51 +0900

    update URL of uv-installer.sh

--- install/common/uv.sh
+++ install/common/uv.sh
@@ -8,7 +8,7 @@ fi
 
 function install_uv() {
     # https://docs.astral.sh/uv/getting-started/installation/#standalone-installer
-    curl -LsSf https://astral.sh/uv/install.sh | sh
+    curl -LsSf https://github.com/astral-sh/uv/releases/latest/download/uv-installer.sh | sh
 }
 
 function uninstall_uv() {

c914d3b83a859911a4b30c6aed84104a3fd00b30
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2024-11-05 15:35:13 +0900

    use `uv` instead of `pyenv` (#238)
    * remove `pyenv` related files and settings
    
    * move `rust.sh` from `~/.local/bin/server` to `~/.local/bin/common`
    
    * install `uv`
    
    * update
    
    * update
    
    * update

---
+++ install/common/uv.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+if [ "${DOTFILES_DEBUG:-}" ]; then
+    set -x
+fi
+
+function install_uv() {
+    # https://docs.astral.sh/uv/getting-started/installation/#standalone-installer
+    curl -LsSf https://astral.sh/uv/install.sh | sh
+}
+
+function uninstall_uv() {
+    # https://docs.astral.sh/uv/getting-started/installation/#uninstallation
+    uv cache clean
+    rm -rf "$(uv python dir)"
+    rm -rf "$(uv tool dir)"
+    rm -f "${HOME}/.cargo/bin/uv" "${HOME}/.cargo/bin/uvx"
+}
+
+function main() {
+    install_uv
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    main
+fi

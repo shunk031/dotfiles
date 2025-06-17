f31ef1bb66c01bbc59b632b32861327bf0e53078
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2025-06-17 16:47:31 +0900

    feat: add functions to install additional brew and cask packages for user shunk031

--- install/macos/common/misc.sh
+++ install/macos/common/misc.sh
@@ -33,6 +33,15 @@ readonly CASK_PACKAGES=(
     vlc
     visual-studio-code
     zotero
+)
+
+# Additional brew packages that I want to install on my personal computer but not on my work computer
+readonly ADDITIONAL_BREW_PACKAGES=(
+    tailscale
+)
+
+# Add applications controlled by the administrator on the work computer here
+readonly ADDITIONAL_CASK_PACKAGES=(
     zoom
 )
 
@@ -78,6 +87,51 @@ function install_brew_cask_packages() {
     fi
 }
 
+function install_additional_brew_packages() {
+    # Only install additional brew packages for user shunk031
+    if [[ "$(whoami)" != "shunk031" ]]; then
+        return 0
+    fi
+
+    local missing_packages=()
+
+    for package in "${ADDITIONAL_BREW_PACKAGES[@]}"; do
+        if ! is_brew_package_installed "${package}"; then
+            missing_packages+=("${package}")
+        fi
+    done
+
+    if [[ ${#missing_packages[@]} -gt 0 ]]; then
+        if "${CI:-false}"; then
+            brew info "${missing_packages[@]}"
+        else
+            brew install --force "${missing_packages[@]}"
+        fi
+    fi
+}
+
+function install_additional_cask_packages() {
+    local missing_packages=()
+
+    for package in "${ADDITIONAL_CASK_PACKAGES[@]}"; do
+        if ! is_brew_package_installed "${package}"; then
+            missing_packages+=("${package}")
+        fi
+    done
+
+    if [[ ${#missing_packages[@]} -gt 0 ]]; then
+        if "${CI:-false}"; then
+            brew info --cask "${missing_packages[@]}"
+        else
+            # Temporarily disable error exit to continue even if some packages fail
+            set +e
+            brew install --cask --force "${missing_packages[@]}"
+            # Re-enable error exit
+            set -e
+        fi
+    fi
+}
+
 function setup_google_chrome() {
     open "/Applications/Google Chrome.app" --args --make-default-browser
 }
@@ -85,6 +139,8 @@ function setup_google_chrome() {
 function main() {
     install_brew_packages
     install_brew_cask_packages
+    install_additional_brew_packages
+    install_additional_cask_packages
 
     # setup_google_chrome
 }

35070b91535e5922159400037999085f063be594
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2025-06-27 00:13:45 +0900

    fix: comment out debug flag in unit test script

--- scripts/run_unit_test.sh
+++ scripts/run_unit_test.sh
@@ -4,7 +4,7 @@ set -Eeuo pipefail
 
 function run_common_test() {
     kcov --clean \
-        --debug-force-bash-stderr \
+        # --debug-force-bash-stderr \
         --include-path=install/common/ \
         "./coverage_common" \
         bats -r "tests/install/common/"
@@ -13,14 +13,14 @@ function run_common_test() {
 function run_os_specific_test() {
     if [ "${OS}" == "macos-14" ]; then
         kcov --clean \
-            --debug-force-bash-stderr \
+            # --debug-force-bash-stderr \
             --include-path=install/macos/common/ \
             "./coverage_macos_common" \
             bats -r "tests/install/macos/common/"
 
     elif [ "${OS}" == "ubuntu-latest" ]; then
         kcov --clean \
-            --debug-force-bash-stderr \
+            # --debug-force-bash-stderr \
             --include-path=install/ubuntu/common/ \
             "./coverage_ubuntu_common" \
             bats -r "tests/install/ubuntu/common/"

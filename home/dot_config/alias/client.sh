d4db095dc11e40b000225e393de9aa7e625e28a9
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2025-09-28 23:55:13 +0900

    fix: update alias for ls command from exa to eza for consistency

--- home/dot_config/alias/client.sh
+++ home/dot_config/alias/client.sh
@@ -4,7 +4,7 @@
 # alias which-neologd='echo `mecab-config --dicdir`"/mecab-ipadic-neologd"'
 # alias ns='watch -n 1 "nvidia-smi"'
 # alias resetport='lsof -t -i tcp:8000 | xargs kill -9'
-alias ls="exa --long --group --header --binary --time-style=long-iso --icons"
+alias ls="eza --long --group --header --binary --time-style=long-iso --icons"
 alias gcloud='docker run --rm -it -v "${HOME%/}"/.config/gcloud:/root/.config/gcloud gcr.io/google.com/cloudsdktool/cloud-sdk gcloud'
 
 # alias for bash-language-server in docker container

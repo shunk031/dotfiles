288e9b58067e6182a0c163b820a7fb9a02ef88e4
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2025-12-03 23:18:44 +0900

    feat: refactor dotenvx installation script to use a variable for binary directory

--- install/common/dotenvx.sh
+++ install/common/dotenvx.sh
@@ -6,10 +6,16 @@ if [ "${DOTFILES_DEBUG:-}" ]; then
     set -x
 fi
 
+readonly BIN_DIR="${HOME}/.local/bin"
+
 function install_dotenvx() {
     local version="1.51.1"
     local url="https://raw.githubusercontent.com/dotenvx/dotenvx/refs/tags/v${version}/install.sh"
-    curl -sfS "${url}?directory=${HOME}/.local/bin&version=${version}" | sh
+    curl -sfS "${url}" | sh -s -- --directory="${BIN_DIR}" --version="${version}"
+}
+
+function uninstall_dotenvx() {
+    rm "${BIN_DIR}/dotenvx"
 }
 
 function main() {

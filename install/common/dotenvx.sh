a4c42e959c3da9d5eafcb4dbe5c4bd73de5c48f9
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2025-12-04 20:28:20 +0900

    fix: ensure binary directory is created before installation

--- install/common/dotenvx.sh
+++ install/common/dotenvx.sh
@@ -9,6 +9,8 @@ fi
 readonly BIN_DIR="${HOME}/.local/bin"
 
 function install_dotenvx() {
+    mkdir -vp "${BIN_DIR}"
+
     local version="1.51.1"
     local url="https://raw.githubusercontent.com/dotenvx/dotenvx/refs/tags/v${version}/install.sh"
     curl -sfS "${url}" | sh -s -- --directory="${BIN_DIR}" --version="${version}"

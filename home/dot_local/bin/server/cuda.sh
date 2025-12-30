2d04a6121f27a5fddb2efd823717d0866ba29b58
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2025-12-30 22:38:03 +0900

    fix: correct path assignment in cuda.sh for proper CUDA bin directory inclusion

--- home/dot_local/bin/server/cuda.sh
+++ home/dot_local/bin/server/cuda.sh
@@ -3,9 +3,8 @@
 export CUDA_HOME="/usr/local/cuda"
 
 typeset -gU path
-path=(
-    $path
-    ${CUDA_HOME}/bin(N-/)
+path+=(
+    ${CUDA_HOME}/bin
 )
 
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$CUDA_HOME/lib64"

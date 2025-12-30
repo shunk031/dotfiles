7030b8541fd1d75ba5dbacfc8237a6575ccf3a49
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2025-12-30 22:49:50 +0900

    Revert "fix: correct path assignment in cuda.sh for proper CUDA bin directory inclusion"
    This reverts commit 2d04a6121f27a5fddb2efd823717d0866ba29b58.

--- home/dot_local/bin/server/cuda.sh
+++ home/dot_local/bin/server/cuda.sh
@@ -3,8 +3,9 @@
 export CUDA_HOME="/usr/local/cuda"
 
 typeset -gU path
-path+=(
-    ${CUDA_HOME}/bin
+path=(
+    $path
+    ${CUDA_HOME}/bin(N-/)
 )
 
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$CUDA_HOME/lib64"

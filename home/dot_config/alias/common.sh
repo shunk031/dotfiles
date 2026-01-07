2420aa77f42f591e8641ff4c93fce83558b67f99
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2026-01-07 12:10:53 +0900

    feat: add alias for claude-mem in common.sh

--- home/dot_config/alias/common.sh
+++ home/dot_config/alias/common.sh
@@ -6,3 +6,6 @@ alias ls="eza --long --group --header --binary --time-style=long-iso --icons"
 # Alias for private chezmoi configuration
 # ref. https://www.chezmoi.io/user-guide/frequently-asked-questions/design/#can-chezmoi-support-multiple-sources-or-multiple-source-states
 alias chezmoi-private="chezmoi --source ~/.local/share/chezmoi-private --config ~/.config/chezmoi-private/chezmoi.yaml"
+
+# Alias for claude-mem
+alias claude-mem='$HOME/.bun/bin/bun "$HOME/.claude/plugins/marketplaces/thedotmack/plugin/scripts/worker-service.cjs"'

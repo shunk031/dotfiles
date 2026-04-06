bcebbea130c9909aec064a26bab5cb646c15a632
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2026-04-06 16:18:03 +0900

    test: scope kcov bash parsing to repository scripts

--- scripts/run_unit_test.sh
+++ scripts/run_unit_test.sh
@@ -2,8 +2,16 @@
 
 set -Eeuo pipefail
 
+readonly KCOV_BASH_OPTIONS=(
+    "--bash-parser=/bin/bash"
+    "--bash-method=DEBUG"
+    "--bash-parse-files-in-dir=${PWD}"
+    "--include-path=${PWD}"
+)
+
 function run_common_test() {
     kcov --clean \
+        "${KCOV_BASH_OPTIONS[@]}" \
         "./coverage_common" \
         bats -r "tests/install/common/"
 }
@@ -11,11 +19,13 @@ function run_common_test() {
 function run_os_specific_test() {
     if [ "${OS}" == "macos-14" ]; then
         kcov --clean \
+            "${KCOV_BASH_OPTIONS[@]}" \
             "./coverage_macos_common" \
             bats -r "tests/install/macos/common/"
 
     elif [ "${OS}" == "ubuntu-latest" ]; then
         kcov --clean \
+            "${KCOV_BASH_OPTIONS[@]}" \
             "./coverage_ubuntu_common" \
             bats -r "tests/install/ubuntu/common/"
     else

716deb562fdd11b31ae1b0c3f7d4c89b65ef44dc
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2026-04-06 16:41:29 +0900

    ci(test): upload codecov only from ubuntu jobs

--- scripts/run_unit_test.sh
+++ scripts/run_unit_test.sh
@@ -2,48 +2,20 @@
 
 set -Eeuo pipefail
 
-function kcov_options() {
-    local -a options=()
-
-    if [ "${OS}" == "macos-14" ]; then
-        options+=("--bash-parser=/bin/bash")
-        options+=("--bash-parse-files-in-dir=${PWD}")
-    fi
-
-    printf '%s\n' "${options[@]}"
-}
-
 function run_common_test() {
-    local -a options=()
-    while IFS= read -r option; do
-        if [ -n "${option}" ]; then
-            options+=("${option}")
-        fi
-    done < <(kcov_options)
-
     kcov --clean \
-        "${options[@]}" \
         "./coverage_common" \
         bats -r "tests/install/common/"
 }
 
 function run_os_specific_test() {
-    local -a options=()
-    while IFS= read -r option; do
-        if [ -n "${option}" ]; then
-            options+=("${option}")
-        fi
-    done < <(kcov_options)
-
     if [ "${OS}" == "macos-14" ]; then
         kcov --clean \
-            "${options[@]}" \
             "./coverage_macos_common" \
             bats -r "tests/install/macos/common/"
 
     elif [ "${OS}" == "ubuntu-latest" ]; then
         kcov --clean \
-            "${options[@]}" \
             "./coverage_ubuntu_common" \
             bats -r "tests/install/ubuntu/common/"
     else
